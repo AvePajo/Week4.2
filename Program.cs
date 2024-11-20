4b8280ae22e8d2ade42d750661e97ab6f34cd960
Ave Pajo <ave.pajo@tktk.ee>
2024-11-21 01:03:39 +0200

    week 4.2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Random rnd = new Random();
+int userGuess;
+
+int numberOfTries = 0;
+//bool loopActive = true;
+
+int cpuRandom = rnd.Next(1, 4);
+
+while (numberOfTries < 3)
+{
+
+    Console.WriteLine("try to guess the cpu number:");
+    userGuess = Convert.ToInt32(Console.ReadLine());
+    numberOfTries++;
+    if(userGuess == cpuRandom)
+    {
+        Console.WriteLine("You won!");
+        break;
+    }
+
+}
+Console.WriteLine($"The number of tries: {numberOfTries}");
+Console.WriteLine("Have a nice day!");
